5dfa1c3d26d2f6d921056fbaf5bf8442dc99286e
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-08-06 19:32:25 -0300

    Testing producer delivery report

--- src/producer.js
+++ src/producer.js
@@ -4,18 +4,43 @@ console.log(Kafka.librdkafkaVersion);
 
 const topic = 'teste';
 
-const producer = new Kafka.Producer({
-  'metadata.broker.list': 'apache-kafka-nodejs-study_kafka_1:9092',
-});
+const producer = new Kafka.Producer(
+  {
+    'metadata.broker.list': 'apache-kafka-nodejs-study_kafka_1:9092',
+    dr_msg_cb: true,
+  },
+  {
+    'request.required.acks': 1,
+  }
+);
 
 producer.connect();
+
+producer.on('delivery-report', (error, report) => {
+  if (error) {
+    console.log('[DELIVERY REPORT]: Error -> ' + error.message);
+    return;
+  }
+  console.log('[DELIVERY REPORT]: ' + formatReport());
+
+  function formatReport() {
+    return `
+    message: ${Buffer.from(report.value).toString()}
+    partition: ${report.partition}
+    offset: ${report.offset}`;
+  }
+});
 
 producer.on('ready', async () => {
   console.log('Connected!');
 
   try {
-    for (let i = 0; i < 5; i++) {
-      producer.produce(topic, null, Buffer.from(`Test message ${i}`), null, Date.now());
+    const list = Array(5)
+      .fill(null)
+      .map((_, i) => i);
+
+    for await (const i of list) {
+      producer.produce(topic, null, Buffer.from(`Test message ${i + 1}`), null, Date.now());
       producer.flush(1000);
       await delay(1);
     }
@@ -24,6 +49,10 @@ producer.on('ready', async () => {
   }
 
   producer.disconnect();
+});
+
+producer.on('event.error', (error) => {
+  console.log('Error on producer: ', error.message);
 });
 
 function delay(seconds) {
