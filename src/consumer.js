dffb5ed55246b118141be0620224a134a3ce2e0b
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-08-17 08:36:55 -0300

    Add consumer

--- src/consumer.js
+++ src/consumer.js
@@ -6,13 +6,17 @@ const consumer = Kafka.KafkaConsumer.createReadStream(
     'group.id': 'kafka',
     'metadata.broker.list': 'apache-kafka-nodejs-study_kafka_1:9092',
   },
-  {},
+  { 'auto.offset.reset': 'earliest' },
   {
     topics: 'teste',
   }
 );
 
 const messages = [];
+
+consumer.on('error', (err) => {
+  console.log('Error: ', err.message);
+});
 
 consumer.on('data', (message) => {
   console.log('Message:', message);
@@ -21,8 +25,12 @@ consumer.on('data', (message) => {
 
 const server = http.createServer((request, response) => {
   for (const m of messages) {
-    response.write(String(m.value) + '\n');
+    response.write(messageFormatted(m));
   }
   response.end();
+
+  function messageFormatted(message) {
+    return `${String(message.value)} - At: ${new Date(message.timestamp).toLocaleString()}\n\n`;
+  }
 });
 server.listen(3333, () => console.log('Listening...'));
